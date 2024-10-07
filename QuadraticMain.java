6a050239d2746d981d6fbbdd03cbe65f357f7b34
Indu7777 <kindu.cs23@bmsce.ac.in>
2024-10-07 19:42:30 +0530

    Add files via upload

--- QuadraticMain.java
+++ QuadraticMain.java
@@ -1,62 +1,55 @@
 import java.util.Scanner;
 class Quadratic{
-
 int a,b,c;
 double r1,r2,d;
-void get()
-{
-Scanner in= new Scanner(System.in);
-System.out.println("enter a");
+void get(){
+Scanner in=new Scanner(System.in);
+System.out.println("Enter the value of a");
 a=in.nextInt();
-System.out.println("enter b");
+System.out.println("Enter the value of b");
 b=in.nextInt();
-System.out.println("enter c");
-c=in.nextInt();
-}
-void compute() 
+System.out.println("Enter the value of c");
+c=in.nextInt();}
+void compute()
 {
-if(a==0) {
-System.out.println("value of a cannot be 0");
+if(a==0)
+{
+System.out.println("The value of a shouldn't be zero");
 }
-else {
+else
+{
 d=(b*b)-(4*a*c);
-
-if(d==0) {
-r1=(-b)/(2*a);
-System.out.print("roots are equal");
-System.out.print("first and seconf roots are "+ r1);
-}
-else if(d>0) {
-r1=(-b + Math.sqrt(d))/(2*a);
-r2=(-b - Math.sqrt(d))/(2*a);
-System.out.print("roots are real and distnct");
-System.out.print("first root is" + r1 + " and second root is " + r2);
-}
-else 
+if(d>0)
 {
-double real=(-b)/(2*a);
-double imaginary=((Math.sqrt(-d)))/(2*a);
-System.out.print("roots are imaginary");
-System.out.print("first root is" + real + "+" + imaginary +"i");
-System.out.print("second root is" + real + "-" + imaginary +"i");
+r1=(-b+Math.sqrt(d))/(2*a);
+r2=(-b-Math.sqrt(d))/(2*a);
+System.out.println("THE ROOTS ARE REAL AND DISTINCT");
+System.out.println("root1="+r1+"Root 2="+r2);
 }
-}
-}
-}
-class QuadraticMain
+else if(d==0)
 {
-public static void main(String s[])
+r1=-b/(2*a);
+System.out.println("THE ROOTS ARE EQUAL");
+System.out.println("root1 = root2 ="+r1);
+}
+else
 {
-Quadratic quad=new Quadratic();
-quad.get();
-quad.compute();
-System.out.print("Name=Jiya Gupta");
-System.out.print("USN=1BM23CS127");
-}
-}
-
-
- 
+double real=-b/(2*a);
+double imag=Math.sqrt(-d)/(2*a);
+System.out.println("THE ROOTS ARE IMAGINARY AND COMPLEX");
+System.out.println("ROOT 1="+real+"+"+imag+"i");
+System.out.println("ROOT 2="+real+"-"+imag+"i");
+}}
+}}
+class QuadraticMain{
+public static void main(String args[])
+{
+Quadratic q=new Quadratic();
+q.get();
+q.compute();
+System.out.println("Name=K INDU");
+System.out.println("USN=1BM23CS131");
+}}
 
 
 
